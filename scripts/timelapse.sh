bc093c0d460e9af950df4cfaa2f41d7e45e37336
Peter Nelson <peter@peterdn.com>
2017-10-10 20:50:50 +0000

    Send telemetry to dweet.io

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -18,3 +18,13 @@ ln -sf "$THUMB_FILE" "$CURRENT_THUMB"
 
 # Update Redis current frame key.
 redis-cli set current.jpg "$IMAGE_FILE"
+
+# Get size of current file in bytes.
+IMAGE_SIZE=$(wc -c < "$IMAGE_FILE")
+
+# Get available space on disk in MB.
+DISK_FREE=$(df "$DATA_DIR" -m --output=avail | tail -n1)
+
+# Dweet telemetry metrics.
+DWEET_THING="gg14-timelapse-pi"
+curl -m 5 "https://dweet.io/dweet/for/$DWEET_THING?timestamp=$TIMESTAMP&imageSize=$IMAGE_SIZE&diskFreeMB=$DISK_FREE"
