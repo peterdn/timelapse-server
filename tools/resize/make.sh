60492489257087c88d6de152600ea15b2b7035ee
Peter Nelson <peter@peterdn.com>
2018-01-28 16:40:07 +0000

    Remove unneeded link libraries

--- tools/resize/make.sh
+++ tools/resize/make.sh
@@ -6,5 +6,5 @@ cc -g -DRASPBERRY_PI -DOMX_SKIP64BIT \
 -I/opt/vc/include/interface/vmcs_host/linux \
 -I/opt/vc/include/IL -I/opt/vc/src/hello_pi/libs/ilclient \
 -L/opt/vc/lib -L/opt/vc/src/hello_pi/libs/ilclient \
--lvchiq_arm -lpthread -lEGL -lGLESv2 omx_resize_image.c component.c \
+-lvchiq_arm -lpthread omx_resize_image.c component.c \
 -o omx_resize_image -lbcm_host -lopenmaxil -lbrcmGLESv2 -lbrcmEGL -lilclient -lvcos -lvchiq_arm -lpthread -pthread -lopenmaxil -lbcm_host
